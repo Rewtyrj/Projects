874a2b0347059f21b564b44f96afe8b844599e6a
Rewtyrj <kutsenkovadim96@gmail.com>
2019-10-03 22:40:49 +0300

    Initial and Create input function.

---
+++ Enia-195/Enia-195/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Enia_195
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Create_input();
+        }
+        static void Create_input()
+        {
+            Console.WriteLine("Введите chisla:/n");
+           string x = Console.ReadLine();
+            try
+            {
+                using (StreamWriter sw =new StreamWriter("@input.txt", true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(x);
+                    Console.WriteLine("Файл записан!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+            
+    }
+}
---
+++ Enia-195/Enia-195/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Enia-195")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DNA Project")]
+[assembly: AssemblyProduct("Enia-195")]
+[assembly: AssemblyCopyright("Copyright © DNA Project 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("16581b88-1c78-4d68-833e-f09903f10cbe")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
